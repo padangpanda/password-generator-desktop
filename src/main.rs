d27d2b3e219f6a2fca54f95b4dede05a582544b3
Padang Pandanaran <ppanda.naran@outlook.com>
2024-11-14 06:55:18 +0700

    refactor - delete set password settings button

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use slint::PlatformError;
-use std::cell::RefCell;
-use std::rc::Rc;
 
 slint::include_modules!();
 
@@ -9,50 +7,18 @@ mod password;
 fn main() -> Result<(), PlatformError> {
     let ui = AppWindow::new()?;
 
-    // Use Rc<RefCell<T>> to allow shared mutable state
-    let len_val = Rc::new(RefCell::new(String::new()));
-    let num_val = Rc::new(RefCell::new(false));
-    let upper_val = Rc::new(RefCell::new(false));
-    let symbol_val = Rc::new(RefCell::new(false));
-
-    // Event handler for setting password settings
-    ui.on_request_set_password_settings({
-        let len_val = Rc::clone(&len_val);
-        let num_val = Rc::clone(&num_val);
-        let upper_val = Rc::clone(&upper_val);
-        let symbol_val = Rc::clone(&symbol_val);
-        
-        move |len, upper, num, symbols| {
-            // Update the values
-            *len_val.borrow_mut() = len.to_string();
-            *num_val.borrow_mut() = num;
-            *upper_val.borrow_mut() = upper;
-            *symbol_val.borrow_mut() = symbols;
-        }
-    });
-
-    // Event handler for generating password
     ui.on_request_generate_password({
         let ui_handle = ui.as_weak();
-        let len_val = Rc::clone(&len_val);
-        let num_val = Rc::clone(&num_val);
-        let upper_val = Rc::clone(&upper_val);
-        let symbol_val = Rc::clone(&symbol_val);
         
-        move || {
+        move |len, upper, num, symbols| {
             let ui = ui_handle.unwrap();
-            let len = len_val.borrow().parse::<i32>().unwrap(); // Parse length
-            let num = *num_val.borrow();  // Access bool value
-            let upper = *upper_val.borrow();  // Access bool value
-            let symbol = *symbol_val.borrow();  // Access bool value
 
-            let pass = password::create_password(len, num, upper, symbol);
+            let pass = password::create_password(len.parse::<i32>().unwrap(), num, upper, symbols);
 
             ui.set_pass(pass.into());
         }
     });
 
-    // Run the UI event loop
     ui.run()?;
 
     Ok(())
